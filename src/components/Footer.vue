d8f356e65f5894bd49a6f22d2970dbb6fb5da92f
torrance-k <knapptori6@gmail.com>
2025-09-16 12:11:27 -0400

    fix: update footer name

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="mt-auto pb-8">
         <div class="container mx-auto px-4 sm:px-6 lg:px-8 text-center text-sm text-slate-700 dark:text-slate-300">
-            <p>© {{ new Date().getFullYear() }} Tori Simmons. Built with Vue and Vite. Deployed to Render.</p>
+            <p>© {{ new Date().getFullYear() }} Tori Knapp. Built with Vue and Vite. Deployed to Render.</p>
         </div>
     </footer>
 </template>
