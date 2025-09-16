0e575c52053d2b4120a644e127c9a108ba4c683c
torrance-k <knapptori6@gmail.com>
2025-09-16 11:59:19 -0400

    fix: add allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,12 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
   ],
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT) : 4173,
+    strictPort: true,
+    allowedHosts: ['porfolio-men9.onrender.com']
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
