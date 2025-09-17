3eda5e08ed88576f7adeaa31b488fdb6b4693cbc
torrance-k <knapptori6@gmail.com>
2025-09-17 13:03:09 -0400

    feat: update skillcloud skills and text color

--- src/components/SkillCloud.vue
+++ src/components/SkillCloud.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="glass p-6">
         <ul class="flex flex-wrap gap-2">
-            <li v-for="s in skills" :key="s" class="badge">{{ s }}</li>
+            <li v-for="s in skills" :key="s" class="badge text-slate-600 dark:text-slate-400">{{ s }}</li>
         </ul>
     </div>
 </template>
@@ -9,6 +9,6 @@
 
 <script setup lang="ts">
 const skills = [
-    'JavaScript', 'TypeScript', 'Vue 3', 'Frontend Development', 'Node.js', 'Cross-Functional Collaboration', 'Vite', 'Jest', 'Tailwind', 'Accessibility', 'Product thinking'
+    'JavaScript', 'HTML', 'CSS', 'TypeScript', 'Vue 3', 'Frontend Development', 'Node.js', 'Cross-Functional Collaboration', 'Vite', 'Jest', 'Tailwind', 'Accessibility', 'Responsive Design', 'Testing and Debugging', 'git & Github'
 ]
 </script>
